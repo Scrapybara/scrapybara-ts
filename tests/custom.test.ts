c4b5e0d4f482366bfb2df806afe1767e4ff06114
Justin Sun <justinsunyt@gmail.com>
2024-12-06 21:45:15 -0800

    update test

--- tests/custom.test.ts
+++ tests/custom.test.ts
@@ -3,9 +3,11 @@ import { ScrapybaraClient } from "../src";
 describe("test", () => {
     it("default", async () => {
         const client = new ScrapybaraClient({
-            authorization: "",
+            apiKey: process.env.SCRAPYBARA_API_KEY ?? "",
         });
         const instance = await client.start();
         await instance.browser.start();
+        await instance.browser.stop();
+        await instance.stop();
     });
 });
