29850058cc0b2875d8c31ac7d8e7443b2d8ff1be
Justin Sun <justinsunyt@gmail.com>
2024-12-07 22:37:59 -0800

    wrapper + test for file, env, notebook, code

--- src/wrapper/ScrapybaraClient.ts
+++ src/wrapper/ScrapybaraClient.ts
@@ -33,6 +33,10 @@ export class Instance {
     public readonly instanceType: string;
     public readonly status: string;
     public readonly browser: Browser;
+    public readonly file: File;
+    public readonly env: Env;
+    public readonly notebook: Notebook;
+    public readonly code: Code;
 
     constructor(id: string, launchTime: Date, instanceType: string, status: string, private readonly fern: FernClient) {
         this.id = id;
@@ -40,6 +44,10 @@ export class Instance {
         this.instanceType = instanceType;
         this.status = status;
         this.browser = new Browser(this.id, this.fern);
+        this.file = new File(this.id, this.fern);
+        this.env = new Env(this.id, this.fern);
+        this.notebook = new Notebook(this.id, this.fern);
+        this.code = new Code(this.id, this.fern);
     }
 
     public async screenshot(
@@ -97,5 +105,94 @@ export class Browser {
 
     public async stop(requestOptions?: FernClient.RequestOptions): Promise<Scrapybara.StopBrowserResponse> {
         return await this.fern.browser.stop(this.instanceId, requestOptions);
+    }
+}
+
+export class File {
+    constructor(private readonly instanceId: string, private readonly fern: FernClient) {}
+
+    public async read(request: Scrapybara.FileReadRequest, requestOptions?: FernClient.RequestOptions) {
+        return await this.fern.file.read(this.instanceId, request, requestOptions);
+    }
+
+    public async write(request: Scrapybara.FileWriteRequest, requestOptions?: FernClient.RequestOptions) {
+        return await this.fern.file.write(this.instanceId, request, requestOptions);
+    }
+
+    public async upload(request: Scrapybara.FileUploadRequest, requestOptions?: FernClient.RequestOptions) {
+        return await this.fern.file.upload(this.instanceId, request, requestOptions);
+    }
+
+    public async download(request: Scrapybara.FileDownloadRequest, requestOptions?: FernClient.RequestOptions) {
+        return await this.fern.file.download(this.instanceId, request, requestOptions);
+    }
+}
+
+export class Env {
+    constructor(private readonly instanceId: string, private readonly fern: FernClient) {}
+
+    public async set(request: Scrapybara.EnvSetRequest, requestOptions?: FernClient.RequestOptions) {
+        return await this.fern.env.set(this.instanceId, request, requestOptions);
+    }
+
+    public async get(requestOptions?: FernClient.RequestOptions) {
+        return await this.fern.env.get(this.instanceId, requestOptions);
+    }
+
+    public async delete(request: Scrapybara.EnvDeleteRequest, requestOptions?: FernClient.RequestOptions) {
+        return await this.fern.env.delete(this.instanceId, request, requestOptions);
+    }
+}
+
+export class Notebook {
+    constructor(private readonly instanceId: string, private readonly fern: FernClient) {}
+
+    public async listKernels(requestOptions?: FernClient.RequestOptions) {
+        return await this.fern.notebook.listKernels(this.instanceId, requestOptions);
+    }
+
+    public async create(request: Scrapybara.CreateNotebookRequest, requestOptions?: FernClient.RequestOptions) {
+        return await this.fern.notebook.create(this.instanceId, request, requestOptions);
+    }
+
+    public async get(notebookId: string, requestOptions?: FernClient.RequestOptions) {
+        return await this.fern.notebook.get(this.instanceId, notebookId, requestOptions);
+    }
+
+    public async delete(notebookId: string, requestOptions?: FernClient.RequestOptions) {
+        return await this.fern.notebook.delete(this.instanceId, notebookId, requestOptions);
+    }
+
+    public async addCell(
+        notebookId: string,
+        request: Scrapybara.AddCellRequest,
+        requestOptions?: FernClient.RequestOptions
+    ) {
+        return await this.fern.notebook.addCell(this.instanceId, notebookId, request, requestOptions);
+    }
+
+    public async executeCell(
+        notebookId: string,
+        cellId: string,
+        request: Scrapybara.ExecuteCellRequest,
+        requestOptions?: FernClient.RequestOptions
+    ) {
+        return await this.fern.notebook.executeCell(this.instanceId, notebookId, cellId, request, requestOptions);
+    }
+
+    public async execute(
+        notebookId: string,
+        request: Scrapybara.ExecuteCellRequest,
+        requestOptions?: FernClient.RequestOptions
+    ) {
+        return await this.fern.notebook.execute(this.instanceId, notebookId, request, requestOptions);
+    }
+}
+
+export class Code {
+    constructor(private readonly instanceId: string, private readonly fern: FernClient) {}
+
+    public async execute(request: Scrapybara.CodeExecuteRequest, requestOptions?: FernClient.RequestOptions) {
+        return await this.fern.code.execute(this.instanceId, request, requestOptions);
     }
 }
--- tests/custom.test.ts
+++ tests/custom.test.ts
@@ -1,13 +1,16 @@
-/**
- * This is a custom test file, if you wish to add more tests
- * to your SDK.
- * Be sure to mark this file in `.fernignore`.
- *
- * If you include example requests/responses in your fern definition,
- * you will have tests automatically generated for you.
- */
+import { ScrapybaraClient } from "../src";
+
 describe("test", () => {
-    it("default", () => {
-        expect(true).toBe(true);
-    });
+    it("default", async () => {
+        const client = new ScrapybaraClient({
+            apiKey: process.env.SCRAPYBARA_API_KEY ?? "scrapy-8bb02c63-1339-4aeb-bae2-b6beea0485cb",
+        });
+        const instance = await client.start();
+        await instance.browser.start();
+        await instance.browser.stop();
+        await instance.code.execute({
+            code: "print('Hello, world!')",
+        });
+        await instance.stop();
+    }, 30000);
 });
