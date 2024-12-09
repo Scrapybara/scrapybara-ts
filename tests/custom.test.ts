d069c9841e9e59694c77ed2953348118895b0d33
Justin Sun <justinsunyt@gmail.com>
2024-12-08 18:07:25 -0800

    new custom test

--- tests/custom.test.ts
+++ tests/custom.test.ts
@@ -1,16 +1,18 @@
+import assert from "assert";
 import { ScrapybaraClient } from "../src";
 
 describe("test", () => {
     it("default", async () => {
+        if (!process.env.SCRAPYBARA_API_KEY) {
+            throw new Error("SCRAPYBARA_API_KEY is not set");
+        }
         const client = new ScrapybaraClient({
-            apiKey: process.env.SCRAPYBARA_API_KEY ?? "",
+            apiKey: process.env.SCRAPYBARA_API_KEY,
         });
         const instance = await client.start();
-        await instance.browser.start();
-        await instance.browser.stop();
-        await instance.code.execute({
-            code: "print('Hello, world!')",
-        });
+        assert(instance.id !== undefined);
+        const screenshotResponse = await instance.screenshot();
+        assert(screenshotResponse.base64Image !== undefined);
         await instance.stop();
     }, 30000);
 });
