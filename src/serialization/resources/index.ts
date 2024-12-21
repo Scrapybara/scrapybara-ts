8b56ab16e1c76f743a4372bb8995485a61f99e29
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-12-21 04:37:09 +0000

    SDK regeneration

--- src/serialization/resources/index.ts
+++ src/serialization/resources/index.ts
@@ -1,11 +1,13 @@
 export * as instance from "./instance";
 export * from "./instance/types";
-export * as file from "./file";
 export * as notebook from "./notebook";
+export * as file from "./file";
 export * from "./instance/client/requests";
+export * as agent from "./agent";
+export * from "./agent/client/requests";
+export * as code from "./code";
+export * from "./code/client/requests";
+export * from "./notebook/client/requests";
 export * from "./file/client/requests";
 export * as env from "./env";
 export * from "./env/client/requests";
-export * from "./notebook/client/requests";
-export * as code from "./code";
-export * from "./code/client/requests";
