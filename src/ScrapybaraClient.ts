0194146bb7a0aa2c929f8fc2965a99a1a8cdf998
Cooper Miller <44559144+kcoopermiller@users.noreply.github.com>
2025-03-11 03:47:22 -0500

    safety checks for ToolCallPart (#34)

--- src/ScrapybaraClient.ts
+++ src/ScrapybaraClient.ts
@@ -346,8 +346,8 @@ export class ScrapybaraClient {
                 headers: {
                     "X-Fern-Language": "JavaScript",
                     "X-Fern-SDK-Name": "scrapybara",
-                    "X-Fern-SDK-Version": "2.3.8",
-                    "User-Agent": "scrapybara/2.3.8",
+                    "X-Fern-SDK-Version": "2.3.9",
+                    "User-Agent": "scrapybara/2.3.9",
                     "X-Fern-Runtime": core.RUNTIME.type,
                     "X-Fern-Runtime-Version": core.RUNTIME.version,
                     ...(await this._getCustomAuthorizationHeaders()),
