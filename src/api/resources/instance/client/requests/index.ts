ee6345b1be6567855f3b624b8aa774ac275af8cb
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-04-08 16:57:40 +0000

    SDK regeneration

--- src/api/resources/instance/client/requests/index.ts
+++ src/api/resources/instance/client/requests/index.ts
@@ -1,6 +1,5 @@
 export { type BashRequest } from "./BashRequest";
 export { type EditRequest } from "./EditRequest";
 export { type FileRequest } from "./FileRequest";
-export { type InstanceDownloadRequest } from "./InstanceDownloadRequest";
 export { type BodyUploadV1InstanceInstanceIdUploadPost } from "./BodyUploadV1InstanceInstanceIdUploadPost";
 export { type InstanceResumeRequest } from "./InstanceResumeRequest";
