e31ed93d35c62b85528c98e65d47d2d71ab23129
Cooper Miller <kcoopermiller9@gmail.com>
2025-04-18 15:12:23 -0700

    2.6.0-beta.2

--- src/api/resources/betaVmManagement/client/Client.ts
+++ src/api/resources/betaVmManagement/client/Client.ts
@@ -54,8 +54,8 @@ export class BetaVmManagement {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "scrapybara",
-                "X-Fern-SDK-Version": "2.6.0-beta.1",
-                "User-Agent": "scrapybara/2.6.0-beta.1",
+                "X-Fern-SDK-Version": "2.6.0-beta.2",
+                "User-Agent": "scrapybara/2.6.0-beta.2",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -136,8 +136,8 @@ export class BetaVmManagement {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "scrapybara",
-                "X-Fern-SDK-Version": "2.6.0-beta.1",
-                "User-Agent": "scrapybara/2.6.0-beta.1",
+                "X-Fern-SDK-Version": "2.6.0-beta.2",
+                "User-Agent": "scrapybara/2.6.0-beta.2",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -218,8 +218,8 @@ export class BetaVmManagement {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "scrapybara",
-                "X-Fern-SDK-Version": "2.6.0-beta.1",
-                "User-Agent": "scrapybara/2.6.0-beta.1",
+                "X-Fern-SDK-Version": "2.6.0-beta.2",
+                "User-Agent": "scrapybara/2.6.0-beta.2",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
